e55ef72bbe0e8b9adaaf95c4b423be2d8b78d39c
edsolater <edsolater@qq.com>
2022-06-12 19:09:59 +0800

    🔧 peerDependence

---
+++ src/react-import.js
@@ -0,0 +1,5 @@
+/**
+ * this file is a workarount for Error: "React is not defined"
+ * @see https://github.com/egoist/tsup/issues/390#issuecomment-933488738
+ */
+export { default as React } from 'react'
